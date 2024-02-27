748d5e953350c610fccd85bdc8af2807b05d7b40
Nada-fuad <naan46511@gmail.com>
2024-02-28 00:03:17 +0100

    versuch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import LineChart from "./LineChart";
+import JsonParentData from "./JsonParentData";
 
 function App() {
   return (
     <div className="App">
       <h1> LinePlot LIVEeooo</h1>
       <LineChart />
+      <JsonParentData />
     </div>
   );
 }
