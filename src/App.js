f5ebc683eed96ec0db6b2b7a4ed054e6ae5cd352
Nada-fuad <naan46511@gmail.com>
2024-02-27 18:41:34 +0100

    wie

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src="/logo.svg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> wie.
         </p>
         <a
           className="App-link"
